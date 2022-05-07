66a1b1b9a104425a7b7a8d758b8c25cbe1fdfa4d
Logan Leatherow <lleatherow@niagarawater.com>
2022-05-07 15:19:35 -0500

    addToZero Done

--- addToZero.js
+++ addToZero.js
@@ -2,3 +2,19 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+
+let value = "false"
+for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+        if (i !== j) 
+            if (array[i] + array[j]===0) {
+                value = "true"
+            }{
+            
+        }
+        
+    }
+    
+}
+
+console.log(value)
